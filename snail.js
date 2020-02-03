091d39af4ba95ad522e018d9568405ae203b0192
Jaymee Liu <{jaymeeliu@gmail.com}>
2020-02-02 22:00:34 -0800

    snail solution

--- snail.js
+++ snail.js
@@ -85,3 +85,79 @@ const snail = (arr, newArr = [], count = 1) => {
 
 //Answer Test 2
 //[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36]
+
+
+
+//Solution 2: Slight Refactor of solution 1
+const snail = (arr, newArr = [], count = 1) => {
+    let tempArr;
+    let tempCount = count;
+
+    //removing first index (aka subArr) of arr and concating subArr into newArr (---> going left)
+    if (arr.length === 0) return newArr;
+    if (count === 1) {
+        tempArr = arr.shift();
+        tempCount++;
+    }
+    // grabbing and removing last index in subArr into newArr from all arr index (going down)
+    else if (count === 2) {
+        tempArr = [];
+        tempCount++;
+        arr.forEach(subArr => {
+            tempArr.push(subArr.pop())
+        })
+    }
+
+    // grabbing and removing last index in arr and reversing subArr into newArr (<--- going right )
+    else if (count === 3) {
+        tempArr = arr.pop().reverse();
+        tempCount++;
+    }
+
+    // grabbing and removing first index in subArr into newArr from all arr index in reverse index order (going up ^)
+    else if (count === 4) {
+        tempArr = []
+        tempCount = 1;
+        arr.forEach(subArr => {
+            tempArr.push(subArr.shift())
+        })
+        tempArr.reverse();
+    }
+
+    return snail(arr, [...newArr, ...tempArr], tempCount)
+}
+
+
+
+//Solution 3: flip and reverse 
+
+const snail = function(array) {
+  const list = [];
+
+  while(array.length) {
+    console.log("a", array)
+    console.log("l", list)
+    list.push(...array.shift(), ...array.map(row => row.pop()));
+     array.reverse().map(row => row.reverse());
+  }
+
+  return list;
+}
+
+
+//Solution 4: Way slower
+
+
+const snail = (array, newArray = []) => {
+    const list = [];
+
+    if (array.length === 0) {
+        return newArray;
+    } else {
+        list.push(...array.shift(), ...array.map(row => row.pop()));
+        array.reverse().map(row => row.reverse());
+
+        return snail(array, [...newArray, ...list])
+    }
+
+}
